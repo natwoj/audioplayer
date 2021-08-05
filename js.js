02a177a27fd643b2066d7eeeb7199b5b216fac23
_natwoj <info@nwojcik.com>
2021-08-05 22:38:58 +0200

    ff rew variables

--- js.js
+++ js.js
@@ -4,6 +4,15 @@ var playerAudio = document.getElementById("player-audio");
 var progress = document.getElementById("progress-on");
 var resTime = 0;
 
+var i = 0;
+var song0 = document.getElementById("song-0");
+var song1 = document.getElementById("song-1");
+var song2 = document.getElementById("song-2");
+var song3 = document.getElementById("song-3");
+var song4 = document.getElementById("song-4");
+var song5 = document.getElementById("song-5");
+
+
 function ChangePlayToPauseButton () {
     playButton.classList.remove("play-button-on");
     playButton.classList.add("play-button-off");
